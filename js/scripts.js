44f3830b174f226f9077b68cd4de89353795f3de
Izadora1 <izadoraandrade83@gmail.com>
2024-04-23 17:04:47 -0300

    centralizado, começo do teste funcional

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 const inputName = document.querySelector("#name");
+const inputEmail = document.querySelector("#email");
+const inputcelular = document.querySelector("#celular");
 const formulario = document.getElementById('meuForm');
 
 
@@ -22,8 +24,28 @@ inputName.addEventListener("keypress", function(){
 
 //impede que o input nome seja enviado vazio
 formulario.addEventListener("submit", function(e){
+    const rgxemail = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+    const rgxcelular = /^\(\d{2}\)\d{9}$/;
+    const rgxdata = /^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])(19|20)\d{2}$/;
+    const rgxcpf = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
     if(inputName.value === ''){
         alert('Por favor, preencha o campo Nome Completo');
+        e.preventDefault();
+    };
+    if(!rgxemail.test(inputEmail.value)){
+        alert('Email inválido, preencha novamente');
+        e.preventDefault();
+    };
+    if(!rgxcelular.test(inputcelular.value)){
+        alert('Número de celular inválido, preencha novamente');
+        e.preventDefault();
+    }
+    if(!rgxdata.test(inputDataNascimento.value)){
+        alert('Data de nascimento inválida, preencha novamente');
+        e.preventDefault();
+    }
+    if(!rgxcpf.test(inputCPF.value)){
+        alert('CPF inválido, preencha novamente');
         e.preventDefault();
     }
 });
